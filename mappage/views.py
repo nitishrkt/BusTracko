3424c0b115380ed9837097961d1cef6f3ac708df
Nitish Sharma <nitishrkt@gmail.com>
2020-03-02 14:41:05 +0530

    added dump file

--- mappage/views.py
+++ mappage/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 from django.urls import reverse
 # Create your views here.
 def handlemap(request):
-    url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBqqW5pq28Qera8ScOykfyKpiIP5Cc0XAM&callback=init"
-    return render(request,'mappage.html', {'link':url})
+    return render(request,'mappage2.html')
